e2ba06e2ac7e1f8281ec391dc0b310fa719a468e
rgb-hmikedev <hmontemayorrios2@gmail.com>
2023-04-01 22:15:06 -0600

    proyecto terminado

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,95 @@
-/* Menu lateral*/
- var menu_visible = false;
- let menu = document.getElementById("nav");
- function mostrarOcultarMenu(){
-    if (menu_visible==false){//si esta oculto
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
         menu.style.display = "block";
-        menu.visible = true;
-  
+        menu_visible = true;
     }
-
     else{
         menu.style.display = "none";
-        menu_visible = false
+        menu_visible = false;
     }
-    
- }
+}
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
 
- //ocultar el menu cuando se selecciona una opcion
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
 
- let links = document.querySelectorAll("nav a");
- for(var x = 0; x <links.length;x++){
-    
- };
+//selecciono todas las barras generales par aluego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let react = document.getElementById("react");
+crearBarra(react);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let uxui = document.getElementById("uxui");
+crearBarra(uxui);
+let figma = document.getElementById("figma");
+crearBarra(figma);
+
+//Ahora voy a guardar la cantidad de barritas que se van a ir pintando por cada barar
+//para eso utilizo un arreglo, cada posiciòn pertenece a un elemento
+//comienzan en -1 porque no tiene ninguna pintada al iniciarse
+let contadores = [-1,-1,-1,-1,-1,-1];
+//esta variable la voy a utilizar de bandera para saber si ya ejecuto la animación
+let entro = false;
+
+//función que aplica las animaciones de la habilidades
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills >=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalUXUI = setInterval(function(){
+            pintarBarra(uxui, 15, 2, intervalUXUI);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 15, 3, intervalPhotoshop);
+        },100);
+        const intervalReact= setInterval(function(){
+            pintarBarra(react, 11, 4, intervalReact);
+        },100);
+        const intervalFigma = setInterval(function(){
+            pintarBarra(figma, 15, 5, intervalFigma);
+        },100);
+    }
+}
+
+//lleno una barra particular con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#43E000";
+    }else{
+        clearInterval(interval)
+    }
+}
+
+//detecto el scrolling del mouse para aplicar la animación de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
